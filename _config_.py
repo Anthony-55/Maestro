ab67c673302fd1bbdbef47a6cf15432440a6dc00
Anthony-55 <37302249+Anthony-55@users.noreply.github.com>
2019-12-20 11:32:53 +0100

    Typo

--- _config_.py
+++ _config_.py
@@ -6,12 +6,12 @@ Fichier pour l'adressage des différents équipement
 '''
 _MQTT_ip='127.0.0.1'				#Adresse IP du broker mqtt
 _MQTT_port=1883						#Port du broker mqtt
-_MQTT_authentication=False          # Mqtt use authentication? 
-_MQTT_user=''			            # Mqtt User name
-_MQTT_pass=''			            # Mqtt password
+_MQTT_authentication=False          #Mqtt use authentication
+_MQTT_user=''			            #Mqtt User name
+_MQTT_pass=''			            #Mqtt password
 _MQTT_TOPIC_SUB='SUBmcz'			#Topic général de souscription
 _MQTT_TOPIC_PUB='PUBmcz'			#Topic général de publication
 _MCZip='192.168.120.1'				#Adresse IP du poêle
 _MCZport='81'						#Port du serveur embarqué du poele
-_VERSION='1.1'						#Version
+_VERSION='1.5'						#Version
 _AUTHOR='Anthony L.'				#Auteur
